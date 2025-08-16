a59ef89f943b9c1d28a76d4ad527026567983204
luiztwill <142128283+luiztwill@users.noreply.github.com>
2025-08-16 09:37:50 -0300

    if e else

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 87
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
