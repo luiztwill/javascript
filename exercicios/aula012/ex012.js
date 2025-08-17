ed660c0ac9a35ddc6c84f70a9440e6fb9482204d
luiztwill <142128283+luiztwill@users.noreply.github.com>
2025-08-17 09:28:08 -0300

    Switch

---
+++ exercicios/aula012/ex012.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+
+var hora = agora.getHours()
+
+console.log (`Agora são exatamente ${hora} horas`)
+if(hora >= 6 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora > 12 && hora <= 18) {
+    console.log('Boa tarde!')
+} else if(hora > 18) {
+    console.log('Boa noite!')
+} else if(hora <= 5) {
+    console.log('Boa madrugada!')
+}
