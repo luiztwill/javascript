e35be2af34fa7aa57368a3b1e6cb5c2180e65c7b
luiztwill <142128283+luiztwill@users.noreply.github.com>
2025-08-19 08:57:16 -0300

    Calculador de idade em JavaScript

---
+++ exercicios/aula012ex/ex015/script.js
@@ -0,0 +1,44 @@
+function verificar() {
+   var data = new Date
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.getElementById('res')
+
+   if (fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert('[ERROR] Verifique os dados e tente novamente!')
+   } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+             genero = 'Homem'
+             if (idade >=0 && idade < 10) { // criança
+                  img.setAttribute('src', 'crianca-homem.png')
+             } else if (idade >= 10 && idade < 21) { // jovem
+                  img.setAttribute('src', 'jovem-homem.png')
+             } else if (idade > 21 && idade < 50) { //adulto
+                  img.setAttribute('src', 'homem-adulto.png')
+             } else { //idoso 
+                  img.setAttribute('src', 'senhor.png')
+             }
+                
+        } else if(fsex[1].checked) {
+             genero = 'Mulher'
+               if (idade >=0 && idade < 10) { // criança
+                  img.setAttribute('src', 'crianca-mulher.png')
+             } else if (idade >= 10 && idade < 21) { // jovem
+                  img.setAttribute('src', 'jovem-mulher.png')
+             } else if (idade > 21 && idade < 50) { //adulta
+                  img.setAttribute('src', 'mulher-adulta.png')
+             } else { //idosa
+                  img.setAttribute('src', 'senhora.png')
+             }
+        } 
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+   }
+   
+}
